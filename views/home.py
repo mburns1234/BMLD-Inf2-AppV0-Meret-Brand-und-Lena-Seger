2e7b9482076615cc9f14a3f2fc600cac1f5b1469
mburns1234 <brandmer@students.zhaw.ch>
2026-02-26 09:45:54 +0100

    Added E-Mails

--- views/home.py
+++ views/home.py
@@ -6,8 +6,8 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Meret Brand (brandmer@students.zhaw.ch)
+- Lena Seger (segerlen@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
