ca5c10e01cee1dcf44d97fb73d61f1faee301150
mburns1234 <brandmer@students.zhaw.ch>
2026-03-04 10:33:03 +0100

    new text

--- views/home.py
+++ views/home.py
@@ -10,6 +10,9 @@ Diese App wurde von folgenden Personen entwickelt:
 - Lena Seger (segerlen@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+Der ECTS‑Notenrechner ermöglicht es, den persönlichen Notendurchschnitt einer Modulgruppe zu berechnen.  \
+Dazu werden für jedes Fach die erzielte Note und die entsprechende ECTS‑Gewichtung eingetragen. \
+Aus diesen Angaben wird automatisch der gewichtete Durchschnitt ermittelt, sodass auf einen Blick ersichtlich ist, ob die Modulgruppe bestanden wurde oder nicht.
 
 Autoren: Meret Brand (brandmer@students.zhaw.ch), Lena Seger (segerlen@students.zhaw.ch)
 """
